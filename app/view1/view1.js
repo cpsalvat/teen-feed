bcef54c0e3f5fcf737e1a6edb66994e51906363e
Christina <c.p.salvatore@gmail.com>
2018-08-08 20:53:05 -0700

    First teen feed app commit

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -5,10 +5,62 @@ angular.module('myApp.view1', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+    controller: 'View1Ctrl as ctrl'
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', LinksPageController]);
 
-}]);
+function LinksPageController($scope) {
+  // Client ID and API key from the Developer Console
+  var CLIENT_ID = '312112159708-o444s9097t0i3g8u0vlanchbs042i186.apps.googleusercontent.com';
+  var API_KEY = 'AIzaSyBMsiOnBapgsFgythe0ngr_3uuQckjTqjs';
+  // Array of API discovery doc URLs for APIs used by the quickstart
+  var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+
+  // Authorization scopes required by the API; multiple scopes can be
+  // included, separated by spaces.
+  var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
+  var LINKS_SPREADSHEET_ID = '1w2JxsDwENhJcobH9Pksvq1XcwaKGEq5CxbrTwOmFKJM';
+
+  this.links = [];
+  this.scope = $scope;
+
+  this.listLinks = function() {
+    gapi.client.sheets.spreadsheets.values.get({
+      spreadsheetId: LINKS_SPREADSHEET_ID,
+      range: 'Links!A2:C',
+    }).then(function(response) {
+      var range = response.result;
+      if (range.values.length > 0) {
+        for (var i = 0; i < range.values.length; i++) {
+          var row = range.values[i];
+          // Print columns A and E, which correspond to indices 0 and 4.
+          this.links.push({
+            title: row[0],
+            url: row[1],
+            summary: row[2]
+          });
+          this.scope.$apply();
+        }
+      } 
+    }.bind(this));
+  }
+
+  this.handleClientLoad = function() {
+    gapi.load('client:auth2', this.initClient.bind(this));
+  }
+
+  this.initClient = function() {
+    gapi.client.init({
+      apiKey: API_KEY,
+      clientId: CLIENT_ID,
+      discoveryDocs: DISCOVERY_DOCS,
+      scope: SCOPES
+    }).then(function() {
+      this.listLinks();
+    }.bind(this));
+  }
+
+  this.handleClientLoad();
+}
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -5,10 +5,90 @@ angular.module('myApp.view2', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {
     templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
+    controller: 'View2Ctrl as ctrl'
   });
 }])
 
-.controller('View2Ctrl', [function() {
+.controller('View2Ctrl', ['$scope', CalendarController]);
 
-}]);
+function CalendarController($scope) {
+  // Client ID and API key from the Developer Console
+  var CLIENT_ID = '312112159708-o444s9097t0i3g8u0vlanchbs042i186.apps.googleusercontent.com';
+  var API_KEY = 'AIzaSyBMsiOnBapgsFgythe0ngr_3uuQckjTqjs';
+  var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+  // Authorization scopes required by the API; multiple scopes can be
+  // included, separated by spaces.
+  var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
+  // Calender ID defined in the calendar settings
+  var CAL_ID = 'cj6g2i651oarhrf56uecbuhtu0@group.calendar.google.com';
+  var MAP_SEARCH_URL =  "https://www.google.com/maps/search/?api=1&query=";
+
+  var MONTH_LIST = 
+      ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+       'August', 'September', 'October', 'November'];
+  
+  this.events = [];
+  this.scope = $scope;
+
+  this.listUpcomingEvents = function() {
+    gapi.client.calendar.events.list({
+      'calendarId': CAL_ID,
+      'timeMin': (new Date()).toISOString(),
+      'showDeleted': false,
+      'singleEvents': true,
+      'maxResults': 10,
+      'orderBy': 'startTime'
+    }).then(function(response) {
+      var eventsList = response.result.items;
+      for (var i=0; i < eventsList.length; i++) {
+        var event = eventsList[i];
+        this.events[i] = {
+          summary: event.summary,
+          locationName: event.location,
+          locationLink: MAP_SEARCH_URL + encodeURIComponent(event.location),
+          date: this.dateToDateString(new Date(event.start.dateTime)),
+          start: this.dateToTimeString(new Date(event.start.dateTime)),
+          end: this.dateToTimeString(new Date(event.end.dateTime)),
+          htmlLink: event.htmlLink,
+          description: event.description,
+        };
+      }
+      this.scope.$apply();
+    }.bind(this));
+  }
+  
+  this.dateToTimeString = function(date) {
+    var options = {
+      hour: 'numeric', 
+      minute: 'numeric',
+    };
+    return new Intl.DateTimeFormat('en-US', options).format(date);
+  }
+  
+  this.dateToDateString = function(date) {
+    var options = { 
+      weekday: 'long', 
+      year: 'numeric', 
+      month: 'long', 
+      day: 'numeric' 
+    };
+    return new Intl.DateTimeFormat('en-US', options).format(date);
+  }
+
+  this.handleClientLoad = function() {
+    gapi.load('client:auth2', this.initClient.bind(this));
+  }
+
+  this.initClient = function() {
+    gapi.client.init({
+      apiKey: API_KEY,
+      clientId: CLIENT_ID,
+      discoveryDocs: DISCOVERY_DOCS,
+      scope: SCOPES
+    }).then(function() {
+      this.listUpcomingEvents();
+    }.bind(this));
+  }
+
+  this.handleClientLoad();
+}
